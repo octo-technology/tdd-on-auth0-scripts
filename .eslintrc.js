64aa5742bf47c5f3c23767d54b8dd32ff6d84b8e
Laurent DUTHEIL <ldutheil@octo.com>
2018-05-26 11:21:56 +0200

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: "eslint:recommended",
+    env: {
+        es6: true,
+        node: true,
+        mocha: true
+    },
+    rules: {
+        "no-unused-vars": "off",
+        "no-undef": "off"
+    }
+};
---
+++ test/.eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    rules: {
+        "no-unused-vars": "error",
+        "no-undef": "error"
+    }
+};
